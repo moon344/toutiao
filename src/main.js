bdc53d17aead6406760362a47cb7e6a6026c93f2
moon <6576934+moon321@user.noreply.gitee.com>
2020-04-23 14:40:01 +0800

    '项目初始化-引入vant组件库'

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant的全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
+
 // 引入全局样式
 import './styles/index.less'
+// 加载动态设置rem基准值
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
